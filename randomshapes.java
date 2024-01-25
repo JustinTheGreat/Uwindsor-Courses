79cf223e5dbc345b87933c28b6767e5eb73aa930
JustinTheGreat <marcusgong123@gmail.com>
2024-01-24 23:02:52 -0500

    randomshapes.java

---
+++ randomshapes.java
@@ -0,0 +1,24 @@
+import java.util.random;
+public class randomshapes{
+    public static void COval(){
+        randNum = new random();
+    }
+    public static void CCircle(){
+
+    }
+    public static void CSquare(){
+
+    }
+    public static void CRectangle(){
+
+    }
+    public static void CCanvas(){
+
+    }
+    public static void CShapes(){
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
