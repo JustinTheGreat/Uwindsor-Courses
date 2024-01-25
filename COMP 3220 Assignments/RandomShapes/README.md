3b64c8f9268e9ee7f5e25818a13edfdf2b8399f1
JustinTheGreat <marcusgong123@gmail.com>
2024-01-24 23:42:20 -0500

    File update

---
+++ COMP 3220 Assignments/RandomShapes/README.md
@@ -0,0 +1,2 @@
+# RandomShapes
+RandomShapes is a random shape dimension generator between 4 shapes: square, rectangle, circle or oval
--- COMP 3220 Assignments/RandomShapes/randomshapes.java
+++ COMP 3220 Assignments/RandomShapes/randomshapes.java
@@ -1,24 +1,66 @@
-import java.util.random;
+import java.util.Random;
 public class randomshapes{
-    public static void COval(){
-        randNum = new random();
+    public static Random rand = new Random();
+    /*
+     * COval() method used to return 2 integers for random oval dimensions (horizontal + vertical radius)
+     */
+    public static String COval(){
+        int rand_int1 = rand.nextInt(101);
+        int rand_int2 = rand.nextInt(101);
+        return ("Oval: " + rand_int1 + " x " + rand_int2);
     }
-    public static void CCircle(){
-
+    /*
+     * CCircle() method used to return 1 integer for random circle radius
+     */
+    public static String CCircle(){
+        int rand_int1 = rand.nextInt(101);
+        return ("Circle: " + rand_int1);
     }
-    public static void CSquare(){
-
+    /*
+     * CSquare() method used to return 1 random integer for random square side dimension
+     */
+    public static String CSquare(){
+        int rand_int1 = rand.nextInt(101);
+        return ("Square: " + rand_int1 + " x " + rand_int1);
     }
-    public static void CRectangle(){
-
+    /*
+     * CRectangle() method used to return 2 random integers for random length and height dimension
+     */
+    public static String CRectangle(){
+        int rand_int1 = rand.nextInt(101);
+        int rand_int2 = rand.nextInt(101);
+        return ("Rectangle: " + rand_int1 + " x " + rand_int2);
     }
+    /*
+     * CShapes() method used to return a singular random shape, returns null if invalid
+     */
+    public static String CShapes(){
+        int roller = rand.nextInt(4);
+        switch (roller) {
+            case 0:
+                return COval();
+            case 1:
+                return CCircle();
+            case 2:
+                return CSquare();
+            case 3:
+                return CRectangle();
+        }
+        return null;
+    }
+    /*
+     * CCanvas function used to canvas 10 shapes.
+     */
     public static void CCanvas(){
-
+        for (int i = 0; i < 10; i++){
+            String shape = CShapes();
+            System.out.println(shape);
+        }
     }
-    public static void CShapes(){
-
-    }
+    /*
+     * Main method used to run CCanvas for demonstration
+     */
     public static void main(String[] args) {
-
+        CCanvas();
     }
 }
